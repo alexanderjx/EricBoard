a9d04fbeb06770fc1050928e9c5e0fb9c6f62fe5
Jux <>
2023-10-26 13:11:11 -0400

    Initial commit

---
+++ articles/admin.py
@@ -0,0 +1,21 @@
+from django.contrib import admin
+from .models import Article, Comment
+
+
+class CommentInline(admin.TabularInline):
+    model = Comment
+
+
+class ArticleAdmin(admin.ModelAdmin):
+    inlines = [
+        CommentInline,
+    ]
+    list_display = [
+        'title',
+        'body',
+        'author',
+    ]
+
+
+admin.site.register(Article, ArticleAdmin)
+admin.site.register(Comment)
