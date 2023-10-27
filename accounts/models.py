4aca40d6eebb3921391eb059cf77e095891db512
Jux <>
2023-10-27 09:08:27 -0400

    update 1

--- accounts/models.py
+++ accounts/models.py
@@ -4,3 +4,12 @@ from django.contrib.auth.models import AbstractUser
 
 class CustomUser(AbstractUser):
     age = models.PositiveIntegerField(null=True, blank=True)
+
+    def is_eric(self):
+        for i in ['Erik', 'Eric', 'Erich', 'Erikk', 'Erick', 'Eirik', 'Eirikr', 'Erikr', 'Aeryk', 'Erk']:
+            if self.first_name == i:
+                return True
+            elif i in self.first_name:
+                return True
+            else:
+                return False
